376e4afe883c9f38ac5e62979ad8eaa3a8b93b74
Yen <git@github.com:TranYen-Roise/FirstPrj2.git>
2024-08-13 14:05:43 +0700

    First commit

---
+++ src/Welcom.java
@@ -0,0 +1,7 @@
+public class Welcom{
+    public static void main(String[] args) {
+        System.out.println("Hello Tran Thi Hai Yen");
+        System.out.println("Welcom to the DHKTPM19A");
+    }
+}
+
